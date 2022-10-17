13b6c6dffdf584ad50971a4ca76c785b1543f63d
Dylan Chapell <d_chapell@coloradocollege.edu>
2022-10-16 19:37:05 -0600

    Make an experiments folder 2 test net.

---
+++ src/experiments/chat2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "nic_app.h"
+
+/**
+ * Program to test two seperate apps operating on each other without interfereing with each other.
+ * Same as chat.c, with a diff app id.
+ */
+char buf[SIZE] = {0};
+uint8_t APP_ID=3;
+
+/**
+ * Print out the message received
+ */ 
+void receiveMessage(uint8_t* message) {
+    printf("%d: %s \n", message[1], &message[6]);
+}
+
+/**
+ * Initialize the router connection and then listen for user input
+ */ 
+int main(){
+    // init nic_app
+    nic_app_init(APP_ID, receiveMessage);
+
+    // get user input
+    while (1){
+        printf("Destination ID: \n");
+        char destString[5];
+        fgets(destString,5,stdin);
+        uint8_t destID = atoi(destString);
+        printf("Message: \n");
+        fgets(buf,SIZE-2,stdin); //will this yell at us? lets find out.
+        
+        send_message(destID,APP_ID,buf,strlen(buf)+1);
+    }
+    return 0;
+}
